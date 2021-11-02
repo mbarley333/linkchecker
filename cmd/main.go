bdedf79d67703a552c6b1d66292628fdee833b48
mbarley333 <jacksonkato808@gmail.com>
2021-11-01 14:27:29 -1000

    RunCli

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"linkchecker"
-	"net/http"
 )
 
 func main() {
-	//linkchecker.RunCLI()
-	results := linkchecker.CheckSiteLinks("example.com",
-		linkchecker.WithBufferedChannelSize(100),
-	)
+	linkchecker.RunCLI()
 
-	for result := range results {
-
-		// output only broken links
-		if result.ResponseCode != http.StatusOK || result.Error != nil {
-			fmt.Println(result)
-		}
-	}
 }
