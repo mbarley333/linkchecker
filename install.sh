70ef3d5c3a5ca77a01e074d6a0ded739b2fe9093
mbarley333 <jacksonkato808@gmail.com>
2021-11-15 08:39:30 -1000

    install.sh

--- install.sh
+++ install.sh
@@ -14,52 +14,10 @@
 # $ curl https://raw.githubusercontent.com/mbarley333/linkchecker/main/install.sh | sh
 # ​
 set -e
-RELEASES_URL="https://github.com/mbarley333/linkchecker/releases"
 
-downloadJSON() {
-    url="$2"
-	
-	echo "Fetching $url.."
-    if test -x "$(command -v curl)"; then
-        response=$(curl -s -L -w 'HTTPSTATUS:%{http_code}' -H 'Accept: application/json' "$url")
-        body=$(echo "$response" | sed -e 's/HTTPSTATUS\:.*//g')
-        code=$(echo "$response" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
-    elif test -x "$(command -v wget)"; then
-        temp=$(mktemp)
-        body=$(wget -q --header='Accept: application/json' -O - --server-response "$url" 2> "$temp")
-        code=$(awk '/^  HTTP/{print $2}' < "$temp" | tail -1)
-        rm "$temp"
-    else
-        echo "Neither curl nor wget was available to perform http requests."
-        exit 1
-    fi
-    if [ "$code" != 200 ]; then
-        echo "Request failed with code $code"
-        exit 1
-    fi
+RELEASES_API="https://api.github.com/repos/mbarley333/linkchecker/releases/latest"
 
-    eval "$1='$body'"
-}
 
-downloadFile() {
-    url="$1"
-    destination="$2"
-
-    echo "Fetching $url.."
-    if test -x "$(command -v curl)"; then
-        code=$(curl -s -w '%{http_code}' -L "$url" -o "$destination")
-    elif test -x "$(command -v wget)"; then
-        code=$(wget -q -O "$destination" --server-response "$url" 2>&1 | awk '/^  HTTP/{print $2}' | tail -1)
-    else
-        echo "Neither curl nor wget was available to perform http requests."
-        exit 1
-    fi
-
-    if [ "$code" != 200 ]; then
-        echo "Request failed with code $code"
-        exit 1
-    fi
-}
 
 findGoBinDirectory() {
     EFFECTIVE_GOPATH=$(go env GOPATH)
@@ -137,42 +95,20 @@ echo "Will install into $INSTALL_DIRECTORY"
 if [ "${OS}" != "linux" ] && { [ "${ARCH}" = "ppc64" ] || [ "${ARCH}" = "ppc64le" ];}; then
     # ppc64 and ppc64le are only supported on Linux.
     echo "${OS}-${ARCH} is not supported by this instalation script"
-else
-    BINARY="linkchecker-${OS}-${ARCH}.tar.gz"
 fi
 
-# add .exe if on windows
-if [ "$OS" = "windows" ]; then
-    BINARY="$BINARY.exe"
-fi
 
-# if DEP_RELEASE_TAG was not provided, assume latest
-if [ -z "$DEP_RELEASE_TAG" ]; then
-    downloadJSON LATEST_RELEASE "$RELEASES_URL/latest"
-    DEP_RELEASE_TAG=$(echo "${LATEST_RELEASE}" | tr -s '\n' ' ' | sed 's/.*"tag_name":"//' | sed 's/".*//' )
-fi
-echo "Release Tag = $DEP_RELEASE_TAG"
+# build binary download url
+jq_cmd=".assets[] | select(.name | endswith(\"${OS}_${ARCH}.tar.gz\")).browser_download_url"
+BINARY_URL="$(curl -s $RELEASES_API | jq -r "${jq_cmd}")"
 
-# fetch the real release data to make sure it exists before we attempt a download
-downloadJSON RELEASE_DATA "$RELEASES_URL/tag/$DEP_RELEASE_TAG"
-
-BINARY_URL="$RELEASES_URL/download/$DEP_RELEASE_TAG/$BINARY"
-DOWNLOAD_FILE=$(mktemp)
-
-downloadFile "$BINARY_URL" "$DOWNLOAD_FILE"
-
-echo "Setting executable permissions."
-chmod +x "$DOWNLOAD_FILE"
-
-INSTALL_NAME="linkchecker"
-
-filename=$(basename $DOWNLOAD_FILE)
+#download binary
+curl -OL ${BINARY_URL}
+filename=$(basename $BINARY_URL)
 tar xvfz ${filename}
-chmod +x ${INSTALL_NAME}
-
-if [ "$OS" = "windows" ]; then
-    INSTALL_NAME="$INSTALL_NAME.exe"
-fi
+filename="linkchecker"
+chmod +x ${filename}
 
 echo "Moving executable to $INSTALL_DIRECTORY/$INSTALL_NAME"
-mv "$DOWNLOAD_FILE" "$INSTALL_DIRECTORY/$INSTALL_NAME"
+mv "$filename" "$INSTALL_DIRECTORY/$INSTALL_NAME"
+
